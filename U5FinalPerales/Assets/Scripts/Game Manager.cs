f1046b40f9650f5a0cce3c541d4fbfc6e376d284
NP36752 <735430@student.dusd.net>
2025-05-16 09:51:18 -0700

    finished main project
    working on bonus features but i havent started the challenge yet

--- U5FinalPerales/Assets/Scripts/Game Manager.cs
+++ U5FinalPerales/Assets/Scripts/Game Manager.cs
@@ -2,24 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
-    private float spawnRate = 1.0f;
+    private float spawnRate = 5.0f;
+    public bool isGameActive;
+    private int score;
+    public GameObject titleScreen;
     public TextMeshProUGUI scoreText;
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI livesText;
+    public Button restartButton;
+    public int lives;
+
+
+    public void StartGame(int difficulty)
     {
         StartCoroutine(SpawnTarget());
+        spawnRate /= difficulty;
+        score = 0;
+        UpdateScore(0);
+        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
+        scoreText.gameObject.SetActive(true);
+        livesText.gameObject.SetActive(true);
+        lives++;
+        UpdateLives();
     }
+    
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive && lives > 0)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
     }
+    
+    public void UpdateScore(int scoreToAdd)
+    {
+        score+= scoreToAdd;
+        scoreText.text = "Score:" + score;
+        
+    }
+
+    public void UpdateLives()
+    {
+       if (isGameActive)
+        {
+            lives--;
+            livesText.text = "Lives:" + lives;
+            if (lives == 0)
+            {
+                GameOver();
+            }
+        }
+    }
+    
+    public void GameOver()
+    {
+        if (lives == 0)
+        {
+            gameOverText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+            isGameActive = false;
+        }        
+    }
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }    
 }
